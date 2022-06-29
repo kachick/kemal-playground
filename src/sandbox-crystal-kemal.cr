76df034cb47e0fb717fbc21864c1ce5709e3149f
Kenichi Kamiya <kachick1@gmail.com>
2022-06-29 23:29:23 +0900

    CORS?

--- src/sandbox-crystal-kemal.cr
+++ src/sandbox-crystal-kemal.cr
@@ -1,5 +1,12 @@
 require "kemal"
 
+static_headers do |response, filepath, filestat|
+  if filepath =~ /\.html$/
+    response.headers.add("Access-Control-Allow-Origin", "*")
+  end
+  response.headers.add("Content-Size", filestat.size.to_s)
+end
+
 get "/" do
   "Hello World!"
 end
