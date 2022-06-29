a51ff3e0c24ef09c0ec83afdc71c1c4c556dda4f
Kenichi Kamiya <kachick1@gmail.com>
2022-06-29 23:30:11 +0900

    Prohibit listing

--- src/sandbox-crystal-kemal.cr
+++ src/sandbox-crystal-kemal.cr
@@ -7,6 +7,8 @@ static_headers do |response, filepath, filestat|
   response.headers.add("Content-Size", filestat.size.to_s)
 end
 
+serve_static({"gzip" => true, "dir_listing" => false})
+
 get "/" do
   "Hello World!"
 end
