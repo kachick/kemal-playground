e881fe6e27f01b77585f2607f736aa50b1c5e2e8
Kenichi Kamiya <kachick1@gmail.com>
2022-06-30 17:41:43 +0900

    Prepare DB

---
+++ migrations/create_todos.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create table todos (
+    id uuid DEFAULT uuid_generate_v4() primary key,
+    subject text,
+    done boolean,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+CREATE UNIQUE INDEX subject_and_done ON todos (id, done);
