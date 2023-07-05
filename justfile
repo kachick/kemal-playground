8ca0ff1a536b5815e0e6e21926b023086ad419ed
Kenichi Kamiya <kachick1@gmail.com>
2023-07-05 23:31:20 +0900

    Implement bone of CRUD

--- justfile
+++ justfile
@@ -7,7 +7,7 @@ dev:
 # https://stackoverflow.com/a/74986582/1212807
 db-start:
   test -d postgres || initdb --pgdata=postgres
-  PGHOST=/tmp pg_ctl --pgdata=postgres --options "-k /tmp" --wait start
+  PGHOST=/tmp pg_ctl --pgdata=postgres --options="-k /tmp" --wait start
 
 db-migrate:
   PGHOST=/tmp psql -d postgres -a -f migrations/create_todos.sql
--- src/kemal-sandbox.cr
+++ src/kemal-sandbox.cr
@@ -1,6 +1,17 @@
 require "kemal"
+require "db"
+require "pg"
 
 module Kemal::Sandbox
+  struct Todo
+    property id, subject, done
+
+    def initialize(@id : UUID, @subject : String, @done : Bool)
+    end
+  end
+
+  database_url = "postgres://kachick@localhost:5432/postgres"
+
   static_headers do |response, filepath, filestat|
     if filepath =~ /\.html$/
       response.headers.add("Access-Control-Allow-Origin", "*")
@@ -10,8 +21,62 @@ module Kemal::Sandbox
 
   serve_static({"gzip" => true, "dir_listing" => false})
 
-  get "/" do
+  get "/ping" do
     "Hello World!"
+  end
+
+  get "/" do
+    todos = [] of Todo
+    DB.open(database_url) do |db|
+      db.query("select id, subject, done from todos") do |rs|
+        rs.each do
+          todos << Todo.new(*rs.read(UUID, String, Bool))
+        end
+      end
+    end
+
+    if todos.empty?
+      render "src/views/error.ecr"
+    else
+      render "src/views/index.ecr", "src/views/application.ecr"
+    end
+  end
+
+  get "/new" do
+    render "src/views/new.ecr"
+  end
+
+  get "/todos/:id" do |env|
+    id = UUID.parse?(env.params.url["id"])
+    unless id
+      render "src/views/error.ecr"
+    end
+
+    todo = DB.open(database_url) do |db|
+      Todo.new(*db.query_one("select id, subject, done from todos where id = $1::uuid", id, as: {UUID, String, Bool}))
+    end
+
+    if todo
+      render "src/views/todos/show.ecr", "src/views/application.ecr"
+    else
+      render "src/views/error.ecr"
+    end
+  end
+
+  get "/todos/:id/edit" do |env|
+    # TOOD: Implement here
+  end
+
+  post "/todos" do |env|
+    subject = env.params.body["subject"]
+    done = env.params.body["done"] == "on"
+    puts [subject, done]
+
+    DB.open(database_url) do |db|
+      db.exec("insert into todos(subject, done) values($1::text, $2::boolean)", subject, done)
+    end
+
+    env.redirect "/"
   end
 end
 
