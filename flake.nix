f21ffa4d2453cdb509bdb528e26afd9322d10dec
Kenichi Kamiya <kachick1@gmail.com>
2024-12-10 10:24:26 +0900

    Update flake to nixos-24.11 and also update crystal tooling

--- flake.nix
+++ flake.nix
@@ -1,52 +1,57 @@
 {
   inputs = {
-    # Candidate channels
-    #   - https://github.com/kachick/anylang-template/issues/17
-    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    selfup = {
+      url = "github:kachick/selfup/v1.1.8";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
+      selfup,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              # https://github.com/kachick/dotfiles/pull/228
-              bashInteractive
-              nixfmt-rfc-style
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            buildInputs =
+              (with pkgs; [
+                bashInteractive
+                findutils # xargs
+                nixfmt-rfc-style
+                nil
 
-              crystal_1_8
-              crystalline
-              ameba
-              shards
-              pcre
-              openssl
-              pkg-config
-              dprint
-              nil
-              just
-              postgresql
-              typos
+                crystal_1_14
+                crystalline
+                ameba
+                shards
+                pcre
+                openssl
+                pkg-config
+                dprint
+                nil
+                just
+                postgresql
+                rainfrog
 
-              # Attempt to use rainfrog after it has binary cache - https://github.com/NixOS/nixpkgs/pull/342487
-            ];
+                dprint
+                typos
+              ])
+              ++ [ selfup.packages.${system}.default ];
           };
-      }
-    );
+        }
+      );
+    };
 }
