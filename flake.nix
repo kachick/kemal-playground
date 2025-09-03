759ebdbffb00c414b7fb05472d157af544e2c8f3
Kenichi Kamiya <kachick1@gmail.com>
2025-09-04 02:13:03 +0900

    Correct crystalline integrations

--- flake.nix
+++ flake.nix
@@ -25,7 +25,11 @@
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          default = pkgs.mkShellNoCC {
+
+          default = pkgs.mkShell {
+            # See https://github.com/crystal-lang-tools/vscode-crystal-lang/pull/209 for detail
+            CRYSTAL_LSP_PATH = lib.getExe pkgs.crystalline;
+
             buildInputs =
               (with pkgs; [
                 bashInteractive
